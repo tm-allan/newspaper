2e026199f3f77774156e1aebdbc97ac8417b5188
allantommathew <allantommathew@gmail.com>
2022-10-12 23:17:44 +0530

    starting from scratch. building new app with classes for generalization. added logging

---
+++ app/logger.py
@@ -0,0 +1,24 @@
+import sys
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+log_file = 'logs.log'
+# if config.DEBUG:
+#     LOG_LEVEL = logging.DEBUG
+# else:
+#     LOG_LEVEL = logging.INFO
+LOG_LEVEL = logging.DEBUG
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(LOG_LEVEL)
+fh = RotatingFileHandler(log_file, maxBytes=10000000, backupCount=3)
+fh.setLevel(LOG_LEVEL)
+# create formatter and add it to the handlers
+formatter = logging.Formatter(fmt='%(levelname).1s %(asctime)s.%(msecs).03d: %(message)s [%(pathname)s:%(lineno)d]', datefmt='%Y-%m-%d %H:%M:%S')
+ch.setFormatter(formatter)
+fh.setFormatter(formatter)
+LOGGER = logging.getLogger('newspaper')
+LOGGER.setLevel(LOG_LEVEL)
+LOGGER.addHandler(ch)
+LOGGER.addHandler(fh)
+LOGGER.propagate = False
--- app/thehindu.py
+++ app/thehindu.py
@@ -1,6 +1,7 @@
 ''' extract articles from thehindu page '''
 import os
 import re
+import logging
 import datetime
 import requests
 from glob import glob
@@ -34,152 +35,95 @@ not_allowed_sections = [
     # 'others',
 ]
 
-
-def get_image(date, file_handler, soup):
-    ''' download image '''
-    try:
-        image_src = soup.select('picture')[0].find_all('source')[0]['srcset']
-        response = requests.get(image_src, timeout=10, headers=headers)
-        # convert the filename into admissible characters
-        basename = "".join(x for x in os.path.basename(image_src) if (x.isalnum() or x=='.'))
-        # move it to {date}/images folder
-        filename = os.path.join(date, 'images', basename)
-        with open(filename, 'wb') as handle:
-            handle.write(response.content)
-        image_alt = soup.select('.lead-img')[0]['alt']
-        # change the filename to remove {date} as the path inside .md file is relative
-        filename = os.path.join('images', basename)
-        file_handler.write(article_image.format(filename, image_alt))
-    except Exception as exc:
-        # print(f'Error4: {str(exc)}')
-        pass
+LOGGER = logging.getLogger('newspaper.thehindu')
 
 
-def get_article(date, file_handler, endpoint):
-    ''' get one news article '''
-    try:
-        response = requests.get(endpoint, timeout=10)
-        if response.status_code != requests.codes.ok:
-            return
-        soup = BeautifulSoup(response.text, 'html.parser')
-        title = soup.select('.title')[0].getText().strip()
-    except Exception as exc:
-        print(f'Error3a: {str(exc)}')
-
-    try:
-        oneliner = soup.select('div.hidden-xs:nth-child(8)')[0].getText().strip()
-    except Exception as exc:
-        # print(f'Error3b: {str(exc)}')
-        oneliner = None
-
-    try:
-        place = soup.select('.ut-container > span:nth-child(1)')[0].getText().strip().upper()
-    except Exception as exc:
-        # print(f'Error3b: {str(exc)}')
-        place = None
-
-    try:
-        value = [int(s) for s in re.findall(r'-?\d+?\d*', os.path.basename(endpoint))][0]
-        data = [str(para) for para in soup.select(f'#content-body-{value}')[0]]
-        # for para in soup.select(f'#content-body-{value}')[0]:
-        #     content = para.getText().strip()
-        #     if len(content.split(' ')) < 10:
-        #         content = f'### {content}'
-        #     data.append(content)
-        data = '\n\n'.join(data)
-    except Exception as exc:
-        print(f'Error3c: {str(exc)}')
-        return
-
-    file_handler.write(article_heading.format(title))
-    get_image(date, file_handler, soup)
-    if oneliner:
-        file_handler.write(article_oneliner.format(oneliner))
-    if place:
-        file_handler.write(f'### {place}\n\n')
-    file_handler.write(article_body.format(data))
-    file_handler.write('-----\n\n')
+class Image():
+    ''' a datastructure to store the details of an image '''
+    def __init__(self, filename, alt_text, raw):
+        self.filename = filename
+        self.alt_text = alt_text
+        self.raw = raw
 
 
-def get_section(date, heading, section_link):
-    ''' download one section of the page '''
-    try:
-        filename = os.path.join(date, heading.replace(' ', '-'))
-        response = requests.get(section_link, timeout=10, headers=headers)
-        # if response.status_code != requests.codes.ok:
-        #     return
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'html.parser')
-        subsections = soup.select('#section_1')[0].select('.archive-list')
-        urls = []
-        for subsection in subsections:
-            # has all the headings and the links
-            for tags in subsection.find_all('a'):
-                # lists all the <a> tags with the links
-                urls += [tags.get('href')]
-
-        with open(f'{filename}.md', 'w') as file_handler:
-            file_handler.write(f'# {heading.title()}\n\n')
-            for url in tqdm(urls, desc=heading):
-                get_article(date, file_handler, url)
-
-        # front_page = soup.select('#section_1')[0].select('.archive-list')[0].find_all('li')
-        # front_page = {item.getText().strip(): item.find('a').get('href') for item in front_page}
-        # # print(front_page)
-        # with open(f'{filename}.md', 'w') as file_handler:
-        #     file_handler.write(f'# {heading}\n\n')
-        #     for link in tqdm(front_page.values(), desc=heading):
-        #         get_article(date, file_handler, link)
-        #         # break
-
-    except Exception as exc:
-        print(f'Error2: {str(exc)}')
-        print(f'Debug Msg: heading: {heading} | section_link: {section_link}')
-        return
+class Article():
+    ''' a datastructure to store the details of an article '''
+    def __init__(self, title, oneliner, place, body):
+        self.title = title
+        self.oneliner = oneliner
+        self.place = place
+        self.body = body
 
 
-def ensure_paths(date):
-    ''' ensure that these paths exists '''
-    if not os.path.exists(date):
-        os.makedirs(date)
-    image_folder = os.path.join(date, 'images')
-    if not os.path.exists(image_folder):
-        os.makedirs(image_folder)
+class TheHinduArticle():
+    def __init__(self, link):
+        self.link = link
+        soup = self.get_page(link)
+        self.content = self.get_content(soup, link)
+        self.image = self.get_image(soup)
 
+    def get_page(self, endpoint):
+        ''' get a page '''
+        try:
+            response = requests.get(endpoint, timeout=10)
+            if response.status_code != requests.codes.ok:
+                return None
+            soup = BeautifulSoup(response.text, 'html.parser')
+        except Exception as exc:
+            LOGGER.error(str(exc))
+            return None
+        return soup
 
-def make_main_page(date):
-    main_page = os.path.join(date, 'main-page.md')
-    with open(main_page, 'w') as handle:
-        handle.write('# The Hindu\n\n')
-        for filename in sorted(glob(f'{date}/*.md')):
-            filename = os.path.basename(filename)
-            heading = os.path.splitext(filename)[0].replace('-', ' ').title()
-            handle.write(f'[{heading}](./{filename})\n\n')
+    def get_image(self, soup):
+        ''' download image '''
+        try:
+            image_src = soup.select('picture')[0].find_all('source')[0]['srcset']
+            response = requests.get(image_src, timeout=10, headers=headers)
+            if response.status_code != requests.codes.ok:
+                return None
+            # convert the filename into admissible characters
+            filename = "".join(x for x in os.path.basename(image_src) if (x.isalnum() or x == '.'))
+            # move it to {date}/images folder
+            image_alt = soup.select('.lead-img')[0]['alt']
+            return Image(filename, image_alt, response.content)
+        except Exception as exc:
+            LOGGER.debug(str(exc))
+            return None
 
+    def get_content(self, soup, link):
+        ''' get one news article '''
+        try:
+            title = soup.select('.title')[0].getText().strip()
+        except Exception as exc:
+            LOGGER.error(str(exc))
 
-def get_mainpage(main_link=main_link):
-    ''' extract details from the page with today's paper '''
-    try:
-        date = datetime.datetime.now().strftime("%y%m%d")
-        response = requests.get(main_link, timeout=10, headers=headers)
-        if response.status_code != requests.codes.ok:
-            return
-        soup = BeautifulSoup(response.text, 'html.parser')
-        header = soup.select('#subnav-tpbar-latest')[0]
-        header = {item.getText().strip().lower(): item.get('href') for item in header}
-        for heading, link in header.items():
-            if heading in not_allowed_sections:
-                continue
-            ensure_paths(date)
-            get_section(date, heading, link)
-        make_main_page(date)
+        try:
+            oneliner = soup.select('div.hidden-xs:nth-child(8)')[0].getText().strip()
+        except Exception as exc:
+            LOGGER.debug(str(exc))
 
-    except Exception as exc:
-        print(f'Error1: {str(exc)}')
-        print(f'header: {header}')
-        return
+        try:
+            place = soup.select('.ut-container > span:nth-child(1)')[0].getText().strip().upper()
+        except Exception as exc:
+            LOGGER.debug(str(exc))
+
+        try:
+            value = [int(s) for s in re.findall(r'-?\d+?\d*', os.path.basename(link))][0]
+            body = [str(para) for para in soup.select(f'#content-body-{value}')[0]]
+            body = '\n\n'.join(body)
+        except Exception as exc:
+            LOGGER.error(str(exc))
+        
+        return Article(title, oneliner, place, body)
 
 
 if __name__ == '__main__':
-    get_mainpage(main_link)
-    # make_main_page('221006')
+    test_article_link = 'https://www.thehindu.com/todays-paper/imf-cuts-indias-growth-forecast-to-68-this-year/article65998939.ece'
+    article = TheHinduArticle(test_article_link)
+    print(f'title: {article.content.title}')
+    print(f'oneliner: {article.content.oneliner}')
+    print(f'place: {article.content.place}')
+    print(f'body: {article.content.body}')
+    print(f'filename: {article.image.filename}')
+    print(f'alt_text: {article.image.alt_text}')
+    # print(article.image.raw)
